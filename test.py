0dd2b8187d1d969f8b6025b86cc1a9909f513a43
jfisher4 <jfisher@broncos.uncfsu.edu>
2016-09-20 15:40:05 -0400

    added flip to lua script and code to dynamically name windows with opencv

--- test.py
+++ test.py
@@ -1,24 +1,38 @@
 import cv2
 import numpy as np
 import lutorpy as lua
+#import the lua script
 require("testing")
 
 lua.LuaRuntime(zero_based_index=True)
 
-
-
+# read in an image
 image2 = cv2.imread('/home/ryanubuntu/Documents/seniorProject/res.jpg',1)
+# print the image type
 print(type(image2))
+# show the original image
 cv2.imshow("initial", image2)
 
+#convert the image to a torch tensor
 x = torch.fromNumpyArray(image2)
 
-w = imageTest(x)
+# create an instance of the imageTest Object
+w = ImageTest
 print(type(w))
 
-y = w.asNumpyArray()
-cv2.imshow("result", y)
+# create a loop to test the lua object
+for i in range(5):
+    tmp = "window_" + str(i)
+    x = w.imageTest(w, x)
+    y = x.asNumpyArray()
+    cv2.imshow(tmp, y)
+# print the lua object attribute calls which tracks how many times we call the image test function
+print(w.calls)
 
+# convert the torch tensor back to a numpy array
+#y = x.asNumpyArray()
+cv2.imshow("result", y)
+# wait for user input to exit and close windows
 k = cv2.waitKey(0)
 if k == 27:         # wait for ESC key to exit
     cv2.destroyAllWindows()
--- testing.lua
+++ testing.lua
@@ -3,12 +3,19 @@ require 'torch'
 
 local py = require('fb.python')
 
+ImageTest = {calls = 0,
+    imageTest = function  (self, img)
+        self.calls = self.calls + 1
+        if self.calls % 2 == 0 then
+            img2 = image.flip(img,2)
+            print("test1")
+        else
+            img2 = image.flip(img,1)
 
-function imageTest (img)
-    print("test1")
-    print(type(img))
-    img = image.vflip(img)
-    print("test2")
-    print(type(img))
-    return img
-end
+            print("test2")
+        end
+
+
+        return img2
+    end
+    }

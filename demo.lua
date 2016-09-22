36e378a40f933e3c46b4d30f126905526807b4be
jfisher4 <jfisher@broncos.uncfsu.edu>
2016-09-22 11:00:37 -0400

    converted the multipathnet code to an object with methods, which when executed in terminal runs without error

--- demo.lua
+++ demo.lua
@@ -18,96 +18,64 @@ local model_utils = require 'models.model_utils'
 local utils = require 'utils'
 local coco = require 'coco'
 
+MultiPathNet = {np=5,si=-2.5,sf=.5,ss=.5,dm=false,thr=0.5,maxsize=600,sharpmask_path='/home/ryanubuntu/multipathnet/data/models/sharpmask.t7',
+    multipath_path='/home/ryanubuntu/multipathnet/data/models/resnet18_integral_coco.t7'}
+--print(MultiPathNet.sharpmask_path)
+MultiPathNet.sharpmask = torch.load(MultiPathNet.sharpmask_path).model
+MultiPathNet.multipathnet = torch.load(MultiPathNet.multipath_path)
 
-
-
-MultiPathNet = {
-
-    np=5,si=-2.5,sf=.5,ss=.5,dm=false,img=nil,thr=0.5,maxsize=600,sharpmask_path='data/models/sharpmask.t7',multipath_path='data/models/resnet18_integral_coco.t7',
-    sharpmask = torch.load(sharpmask_path).model
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-local sharpmask = torch.load(config.sharpmask_path).model
-sharpmask:inference(config.np)
-
-local multipathnet = torch.load(config.multipath_path)
-multipathnet:evaluate()
-multipathnet:cuda()
-model_utils.testModel(multipathnet)
-
-local detector = fbcoco.ImageDetect(multipathnet, model_utils.ImagenetTransformer())
-
-------------------- Run DeepMask --------------------
-
-local meanstd = {mean = { 0.485, 0.456, 0.406 }, std = { 0.229, 0.224, 0.225 }}
-
-local scales = {}
-for i = config.si,config.sf,config.ss do table.insert(scales,2^i) end
-print(scales)
-
-local infer = Infer{
-  np = config.np,
-  scales = scales,
-  meanstd = meanstd,
-  model = sharpmask,
-  dm = config.dm,
+MultiPathNet.meanstd = {mean = { 0.485, 0.456, 0.406 }, std = { 0.229, 0.224, 0.225 }}
+MultiPathNet.scales = {}
+MultiPathNet.infer = Infer{
+    np = MultiPathNet.np,
+    scales = MultiPathNet.scales,
+    meanstd = MultiPathNet.meanstd,
+    model = MultiPathNet.sharpmask,
+    dm = MultiPathNet.dm,
 }
-
-local img = image.load(config.img)
-img = image.scale(img, config.maxsize)
-local h,w = img:size(2),img:size(3)
-
-infer:forward(img)
-
-local masks,_ = infer:getTopProps(.2,h,w)
-
-local Rs = coco.MaskApi.encode(masks)
-local bboxes = coco.MaskApi.toBbox(Rs)
-bboxes:narrow(2,3,2):add(bboxes:narrow(2,1,2)) -- convert from x,y,w,h to x1,y1,x2,y2
-
-------------------- Run MultiPathNet --------------------
-
-local detections = detector:detect(img:float(), bboxes:float())
-local prob, maxes = detections:max(2)
-
--- remove background detections
-local idx = maxes:squeeze():gt(1):cmul(prob:gt(config.thr)):nonzero():select(2,1)
-bboxes = bboxes:index(1, idx)
-maxes = maxes:index(1, idx)
-prob = prob:index(1, idx)
-
-local scored_boxes = torch.cat(bboxes:float(), prob:float(), 2)
-local final_idx = utils.nms_dense(scored_boxes, 0.3)
-
-------------------- Draw detections --------------------
-
--- remove suppressed masks
-masks = masks:index(1, idx):index(1, final_idx)
-
-local dataset = paths.dofile'./DataSetJSON.lua':create'coco_val2014'
-
-local res = img:clone()
-coco.MaskApi.drawMasks(res, masks, 10)
-for i,v in ipairs(final_idx:totable()) do
-   local class = maxes[v][1]-1
-   local x1,y1,x2,y2 = table.unpack(bboxes[v]:totable())
-   y2 = math.min(y2, res:size(2)) - 10
-   local name = dataset.dataset.categories[class]
-   print(prob[v][1], class, name)
-   image.drawText(res, name, x1, y2, {bg={255,255,255}, inplace=true})
+MultiPathNet.start = function (self)
+    self.sharpmask:inference(self.np)
+    self.multipathnet:evaluate()
+    self.multipathnet:cuda()
+    model_utils.testModel(self.multipathnet)
+    self.detector = fbcoco.ImageDetect(self.multipathnet, model_utils.ImagenetTransformer())
+    for i = self.si,self.sf,self.ss do table.insert(self.scales,2^i) end
+    print(scales)
+    self.infer.scales = self.scales -- update the scales
 end
-image.save(string.format('./res.jpg',config.model),res)
+MultiPathNet.processImg = function (self, img)
+    img = image.scale(img, self.maxsize)
+    h,w = img:size(2),img:size(3)
+    self.infer:forward(img)
+    masks,_ = self.infer:getTopProps(.2,h,w)
+    Rs = coco.MaskApi.encode(masks)
+    bboxes = coco.MaskApi.toBbox(Rs)
+    bboxes:narrow(2,3,2):add(bboxes:narrow(2,1,2)) -- convert from x,y,w,h to x1,y1,x2,y2
+    detections = self.detector:detect(img:float(), bboxes:float())
+    prob, maxes = detections:max(2)
+    -- remove background detections
+    idx = maxes:squeeze():gt(1):cmul(prob:gt(self.thr)):nonzero():select(2,1)
+    bboxes = bboxes:index(1, idx)
+    maxes = maxes:index(1, idx)
+    prob = prob:index(1, idx)
+    scored_boxes = torch.cat(bboxes:float(), prob:float(), 2)
+    final_idx = utils.nms_dense(scored_boxes, 0.3)
+    -- remove suppressed masks
+    masks = masks:index(1, idx):index(1, final_idx)
+    dataset = paths.dofile'./DataSetJSON.lua':create'coco_val2014'
+    --make a table to hold all of the names
+    names = {}
+    coco.MaskApi.drawMasks(res, masks, 10)
+    for i,v in ipairs(final_idx:totable()) do
+        class = maxes[v][1]-1
+        name = dataset.dataset.categories[class]
+        -- insert the name into the table
+        table.insert(names,name,i)
+        print(prob[v][1], class, name)
 
-print('| done')
+    end
+
+    print('| done')
+    return prob, names, masks
+end
+

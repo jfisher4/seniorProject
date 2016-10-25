09b60a59ff84ef5f5294446c47b2eda33ccdd03e
Ryan Fisher <jfisher4@broncos.uncfsu.edu>
2016-10-25 11:27:20 -0400

    minor changes for testing

--- multipathnetStuff/run.py
+++ multipathnetStuff/run.py
@@ -25,4 +25,5 @@ def run(directory,videoname):
 
 #for i in range(len(goodVideoList)):
     #run("/home/robotics_group/multipathnet/deepmask/data/",goodVideoList[i])
-run("C:\\Users\\gifis\\Documents\\seniorProject\\multipathnetStuff\\","00028.MTS")
+#run("C:\\Users\\gifis\\Documents\\seniorProject\\multipathnetStuff\\","00028.MTS") # for ryan's windows laptop
+run("/home/robotics_group/multipathnet/deepmask/data/","00004.MTS")
